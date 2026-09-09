9c9d64832af24d342b9c7c92b762b9a960ef606e
Christian Sullivan <chrisullivan@nvidia.com>
2026-09-09 16:21:07 -0400

    Update for CUDA 13.4.1

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,8 +1,14 @@
+if "%TARGET_PLATFORM%" == "win-arm64" (
+    set CUDA_ARCH=arm64
+) else (
+    set CUDA_ARCH=x64
+)
+
 if not exist %PREFIX% mkdir %PREFIX%
-if not exist %LIBRARY_LIB%\x64 mkdir %LIBRARY_LIB%\x64
+if not exist %LIBRARY_LIB%\%CUDA_ARCH% mkdir %LIBRARY_LIB%\%CUDA_ARCH%
 mkdir %PREFIX%/cuda-cupti
 
-move lib\*.lib %LIBRARY_LIB%\x64
+move lib\*.lib %LIBRARY_LIB%\%CUDA_ARCH%
 move lib\*.dll %LIBRARY_BIN%
 move include\* %LIBRARY_INC%
 move doc %PREFIX%
